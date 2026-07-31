0ffff3498d0106a11e8344ea5c59c6a98dade1e1
Harshit-Yadav0 <harshit.codesmith@gmail.com>
2026-07-31 22:16:05 +0530

    Updated ChemoraPy

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ i = 1
 # Open the file once outside the loop for better performance
 with open("shielding_const.txt", "a") as file:
     while i <= 118:
-        result = cm.shealding_constant(i)
+        result = cm.shielding_constant(i)
         file.write(str(result) + "\n") 
         i += 1  # Increment to avoid infinite loop
                                                     
