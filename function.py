446a5c19c1bf43a68a610f40b792946cd7dea62a
Harshit-Yadav0 <harshit.codesmith@gmail.com>
2026-07-31 11:18:53 +0530

    Updated ChemoraPy

--- function.py
+++ function.py
@@ -1,2 +1,26 @@
-def aafbau(z):
-  
+def aafbau(Z):
+
+    orbitals = [
+        ("1s", 2), ("2s", 2), ("2p", 6),
+        ("3s", 2), ("3p", 6), ("4s", 2),
+        ("3d", 10), ("4p", 6), ("5s", 2),
+        ("4d", 10), ("5p", 6), ("6s", 2),
+        ("4f", 14), ("5d", 10), ("6p", 6),
+        ("7s", 2), ("5f", 14), ("6d", 10),
+        ("7p", 6)
+    ]
+
+    configuration = []
+
+    for orbital, capacity in orbitals:
+
+        if Z <= 0:
+            break
+
+        electrons = min(Z, capacity)
+
+        configuration.append((orbital, electrons))
+
+        Z -= electrons
+
+    return configuration
